4344301a2f8fca1e843438c2611bb63cdf7273b7
AidenRF-ai <kolodec16@gmail.com>
2026-08-20 23:42:42 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
 import "./styles/global.css";
 
+import { ModelProvider } from "./context/ModelContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ModelProvider>
+      <App />
+    </ModelProvider>
   </React.StrictMode>
 );
