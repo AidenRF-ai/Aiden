53bd2241b0448157b7181a6ec1e5227509e1e9ac
AidenRF-ai <kolodec16@gmail.com>
2026-08-21 00:17:52 +0300

    Update Input.jsx

--- frontend/src/components/UI/Input.jsx
+++ frontend/src/components/UI/Input.jsx
@@ -1,1 +1,14 @@
-
+export default function Input({
+  value,
+  onChange,
+  placeholder
+}) {
+  return (
+    <input
+      className="aiden-input"
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+    />
+  );
+}
