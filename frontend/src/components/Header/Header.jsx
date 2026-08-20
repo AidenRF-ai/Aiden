3b7e0100b532ecb9c4122137a1e49800586702e6
AidenRF-ai <kolodec16@gmail.com>
2026-08-20 22:55:57 +0300

    Create Header.jsx

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import ModelSelector from "../ModelSelector/ModelSelector";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Aiden AI</h1>
+
+      <ModelSelector />
+    </header>
+  );
+}
